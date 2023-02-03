a0e1b8afeb251befb3eda04f329454a28f319499
Incognito-Guy <headset.hero.viraj01@gmail.com>
2023-02-03 10:02:00 +0530

    Update index.js

--- about/index.js
+++ about/index.js
@@ -1,12 +1,12 @@
 const subtitles = document.querySelectorAll(`.card-subtitle`);
 
 texts = [
-    'Team Leader, App Page, Sustainable Cities App, CSS Design & Home Page Design',
-    'Home Page Content, Health Page Content',
+    'Team Leader, Sustainable Cities App, App Page Content, All Page Designs, Carousel Designs',
+    'Home Page Content, Navigation Bar Design',
     'About Us Page Content',
-    'Health Content',
+    'Health Page Content',
     'Poster Creation',
-    'Energy Creation'
+    'Energy Page Content'
 ]
 
 let subtitleIndex = 0;
@@ -18,7 +18,7 @@ for (const subtitle of subtitles) {
     
         word.classList.add(`card-subtitle-word`);
     
-        word.style.transitionDelay = `${index * 50}ms`;
+        word.style.transitionDelay = `${index * 40}ms`;
     
         return word;
     }
@@ -34,6 +34,7 @@ for (const subtitle of subtitles) {
 const cards = document.querySelectorAll('.card');
 
 activeCardIndex = 0;
+cards[0].style.display = 'block';
 
 const refresh = () => {
     if (activeCardIndex < 0) {
@@ -45,14 +46,49 @@ const refresh = () => {
     for (const card of cards) {
         card.style.display = 'none';
     }
-    cards[activeCardIndex].style.display = 'block'
+    cards[activeCardIndex].style.display = 'block';
+    cards[activeCardIndex].style.left = 0;
 }
 
-document.querySelector('.next').addEventListener('click', () => {
-    activeCardIndex++;
-    refresh();
-});
-document.querySelector('.prev').addEventListener('click', () => {
-    activeCardIndex--;
-    refresh();
-});
+let hoveredcard;
+let mouseOver = false;
+let mouseDown = false
+let mouseX = 0;
+
+for (const card of cards) {
+    card.addEventListener('mouseover', () => {
+        hoveredcard = card;
+        mouseOver = true;
+    });
+    card.addEventListener('mouseleave', () => {
+        mouseOver = false;
+    });
+}
+
+window.onmousedown = e => {
+    mouseDown = true
+    mouseX = e.clientX;
+}
+
+window.onmousemove = e => {
+    if (mouseDown) {
+        let distance = e.clientX - mouseX;
+        if (distance < 100 && distance > -100) {
+            hoveredcard.style.left = distance;
+        }
+    }
+}
+
+window.onmouseup = e => {
+    let distance = e.clientX - mouseX;
+    if (distance > 50) {
+        activeCardIndex--;
+        refresh();
+    } else if (distance < -50) {
+        activeCardIndex++;
+        refresh();
+    } else {
+        hoveredcard.style.left = 0;
+    }
+    mouseDown = false
+}
