ace1913e9a66569442d4110207e49054f7de3689
Mani_Dev <56951627+Mani-cwaf@users.noreply.github.com>
2023-02-04 10:44:13 +0530

    Update index.js

--- about/index.js
+++ about/index.js
@@ -2,7 +2,7 @@ const subtitles = document.querySelectorAll(`.card-subtitle`);
 
 texts = [
     'Sustainable Cities App, App Page Content, All Page Designs, Navigation Menu, Home Page Content, About The Team',
-    'Health Page Content, Played venge.io',
+    'Health Page Content',
     'About Page Content',
     'Health Page',
     'Poster Creation',
